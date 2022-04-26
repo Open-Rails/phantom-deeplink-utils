9f0d23bb4d6d1ce7aa16ff753d44e6f9d1416aca
Paul Fidika <paul@fidika.com>
2022-04-26 17:55:01 -0600

    remove reference to mjs files

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
-// import { whatever } from 'phantom-deeplink-utils'
 import something from 'phantom-deeplink-utils'
 
 function App() {
